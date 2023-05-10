cc500e24925adc4f074bd1e24336ab7d2b150529
eons <eons@eons.dev>
2023-05-09 17:16:33 -0700

    init

---
+++ src/elgato_ring_light.py
@@ -0,0 +1,28 @@
+from ebbs import Builder
+import requests
+import logging
+
+class elgato_ring_light(Builder):
+	def __init__(this, name="Elgato Ring Light"):
+		super().__init__(name)
+
+		this.requiredKWArgs.append("elgato_ring_light_ip")
+
+		this.optionalKWArgs['brightness'] = 50
+		this.optionalKWArgs['temperature'] = 4000
+		this.optionalKWArgs['on'] = True
+
+	def Build(this):
+		requestData = {
+			"lights": [
+				{
+					"on": int(this.on),
+					"brightness": this.brightness,
+					"temperature": this.temperature
+				}
+			]
+		}
+
+		r = requests.put(f"http://{this.elgato_ring_light_ip}:9123/elgato/lights", json=requestData)
+		logging.debug(f"Sent: {r.request.data} with headers: {r.request.headers}")
+		logging.debug(f"Elgato Ring Light Response ({r.status_code}): {r.text}")
